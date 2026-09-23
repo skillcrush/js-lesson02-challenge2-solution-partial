98c87a2a6e2e82f32856d5394313744f301c35a3
tauri_s <tauristclaire@gmail.com>
2026-09-23 13:01:31 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var mealCost = 25.99;
+
+var tip = 0.18;
+
+var tipAmount = mealCost * tip;
+var total = mealCost + tipAmount;
+
+var numOfPeople = 2;
+
+console.log(
+  `Your meal cost $${mealCost}, and with the tip, it comes to $${total.toFixed(
+    2
+  )}. Your share is $${(total / numOfPeople).toFixed(
+    2
+  )}. Hope it was delicious!`
+);
